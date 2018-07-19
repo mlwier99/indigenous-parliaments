0ebc108ffe1eb8d9296940d222f7e88c108af85e
Eleonore Fournier-Tombs <eleonoreft@Eleonores-Air.local>
2018-07-19 10:04:07 -0400

    parser improvements

--- Nunavut/get_nunavut_hansards.py
+++ Nunavut/get_nunavut_hansards.py
@@ -1,7 +1,8 @@
 import sys
 import requests
-import PyPDF2 as pypdf2
 import pandas as pd
+import textract
+import datetime
 
 def get_pdf(date):
     # sometimes the files have a "_0" associated with them
@@ -13,48 +14,51 @@ def get_pdf(date):
     return file
 
 def extract_speakers(pdf, first_page):
-   titles = ["Hon.", "Mr.", "Ms.", "Speaker"]
    speaker_list = []
-   clean_page = first_page[first_page.find("Members Present"):first_page.find(">>House")]
-   clean_page = clean_page.replace(",","").replace("\n","").replace("Mr. Speaker", "mrspeaker").replace(">>Applause","")
-   for t in titles:
-       clean_page = clean_page.replace(t, "* " + t)
-   clean_page = clean_page.split("*")
+   clean_page = first_page[first_page.find("Members Present"):first_page.find(">>House")].replace("\n"," ")
+   clean_page = clean_page.split(", ")
+   titles = ["Ms.", "Mr.", "Hon."]
    for k, v in enumerate(clean_page):
+       speaker_list.append(v)
+       speaker_words = v.split(" ")
        for t in titles:
            if t in v:
-               speaker_list.append(v)
+               speaker_list.append(speaker_words[0] + " " + speaker_words[2].replace(".",""))
+           else:
+               speaker_list.append(t + " " + speaker_words[1].replace(".",""))
    return speaker_list
 
-def process_pdf(pdf):
-    pdf_obj = open(pdf, 'rb')
-    pdf_reader = pypdf2.PdfFileReader(pdf_obj)
-    print("Number of pages in Hansard: " + str(pdf_reader.numPages))
+def process_pdf(pdf,date):
+    clean_page = textract.process(pdf)
     clean_df = pd.DataFrame(columns=["speaker","speech"])
-    titles = ["Hon.", "Mr.", "Ms.", "Speaker"]
     speaker_list = None
-    for p in range(0, pdf_reader.numPages):
-        page_obj = pdf_reader.getPage(p) 
-        raw_page = page_obj.extractText()
-        raw_page = raw_page.replace("\n", "")
-        if "Members Present:" in raw_page:
-            print(raw_page)
-            speaker_list = extract_speakers(pdf, raw_page)
-            print(speaker_list)
-        elif speaker_list == None:
-            print("Skipped cover page: " + str(p))
-        else:
-            clean_page = raw_page.replace(",","").replace("Mr. Speaker", "mrspeaker").replace(">>Applause","")
-            clean_page = clean_page.replace(":",",")
-            clean_page = clean_page.split(",")
-            for k,v in enumerate(clean_page):
-                for t in speaker_list:
-                    if t in v:
-                        try:
-                            df = pd.DataFrame(data={"speaker":[t], "speech":[clean_page[k+1]]})
-                            clean_df = clean_df.append(df, ignore_index=True)
-                        except IndexError:
-                            print(v)
+    speaker_list = extract_speakers(pdf, clean_page)
+    print(speaker_list)
+    clean_page = clean_page[clean_page.find("Opening Prayer"):]
+    clean_page = clean_page.replace("Thank you, Mr. Speaker.", "").replace("Thank you, Mr. Chairman.","").replace(">>Applause","").replace(">>Laughter", "").replace("\n"," ").replace(" (interpretation)","").replace("(interpretation ends)","").replace("Nunavut Hansard ","")
+    # Strip date Thursday, May 23, 2018
+    date = str(date)
+    year = int(date[:4])
+    month = int(date[4:6])
+    day = int(date[6:8])
+    hansard_date = datetime.datetime(year, month, day)
+    date_string = hansard_date.strftime("%A, %B %-d, %Y")
+    clean_page = clean_page.replace(date_string, "")
+    for s in speaker_list:
+        clean_page = clean_page.replace(s+":",s+":**")
+    clean_page = clean_page.replace("Speaker:","Speaker:**")
+    clean_page = clean_page.replace("Chairman:","**Chairman:**")
+    #clean_page = clean_page.replace(":",":**")
+    clean_page = clean_page.split("**")
+    for k,v in enumerate(clean_page):
+        for t in speaker_list:
+            if t+":" in v:
+                try:
+                    df = pd.DataFrame(data={"speaker":[t], "speech":[clean_page[k+1]]})
+                    clean_df = clean_df.append(df, ignore_index=True)
+                except IndexError:
+                    print(v)
+    clean_df['speech'] = clean_df['speech'].apply(lambda x: x.replace("Speaker:",""))
     print(clean_df)
     return clean_df
 
@@ -63,8 +67,8 @@ def process_pdf(pdf):
 def main(date=sys.argv[1]):
     # Add a loop for this to check and add for files automatically
     pdf_name = get_pdf(date)
-    hansard_df = process_pdf(pdf_name)
-    hansard_df.to_csv("./clean_csvs/"+date+".csv")
+    hansard_df = process_pdf(pdf_name,date)
+    hansard_df.to_csv("./clean_csvs/"+date+".csv", encoding='utf-8-sig')
     print("CSV saved for: " + date)
 
 if __name__=='__main__':
